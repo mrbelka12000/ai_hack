77b166c9b0e252be778eb6dd760bbc0a7cc0a49e
beknur.karshyga <beknur.karshyga@armenotech.com>
2025-06-13 23:22:26 +0500

    docs update

--- docs/docs.go
+++ docs/docs.go
@@ -909,7 +909,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "aiport.mrbelka12000.com",
+	Host:             "aihack.mrbelka12000.com",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger Example API",
