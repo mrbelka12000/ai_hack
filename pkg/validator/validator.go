f06e9d3836fe33aebe417a70def5310643bd500f
beknur.karshyga <beknur.karshyga@armenotech.com>
2025-06-13 10:54:52 +0500

    update validator

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -1,9 +1,15 @@
 package validator
 
-import "github.com/go-playground/validator/v10"
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var validate = validator.New(validator.WithRequiredStructEnabled())
 
 func ValidateStruct(obj interface{}) error {
+	log.Println(fmt.Sprintf("%+v", obj))
 	return validate.Struct(obj)
 }
