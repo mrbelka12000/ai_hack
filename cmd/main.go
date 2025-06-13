8be746e1f96792a0ef7d7eb9d570d9ac752b10e4
beknur.karshyga <beknur.karshyga@armenotech.com>
2025-06-14 01:15:59 +0500

    docs update

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8085
+// @host      aihack.mrbelka12000.com
 // @BasePath  /api/v1
 
 // @securityDefinitions.apikey Bearer
